53596ad0e0f8004392316ad6b9bf22a11e39f8a2
MarquesMat <marques_matheus@id.uff.br>
2024-12-10 21:52:04 -0300

    Adicionando exemplo mais completo

--- ex1.java
+++ ex1.java
@@ -1,13 +1,20 @@
-class Factorial{
-    public static void main(String[] a){
-        a = ;
+class Factorial {
+    public static void main(String[] a) {
+        System.out.println(new Fac().ComputeFac(10));
     }
-   }
+}
+// Comentário de uma linha
 class Fac {
-    int a;
-    int b;
-    public int ComputeFac(int num){
-    if (){
+    public int ComputeFac(int num) {
+        int num_aux;
+        if (num < 1) // Ignorar essa parte
+            num_aux = 1;
+        else
+            num_aux = num * (this.ComputeFac(num - 1));
+        return num_aux;
     }
-    }
-}   
+    /*
+     * Comentário de várias linhas
+     * Remover aqui
+     */
+}
