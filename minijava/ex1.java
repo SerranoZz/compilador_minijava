794b80b9fda83e7c8657624b32f0571d08197186
Lucas Serrano <lucas_serrano@id.uff.br>
2024-12-30 16:41:24 -0300

    enviando

--- minijava/ex1.java
+++ minijava/ex1.java
@@ -1,17 +1,29 @@
 class Factorial{
     public static void main(String[] a){
-        System.out.println(new Fac().ComputeFac(10));
+        System.out.println(new Fac1().ComputeFac(10));
     }
 }
-class Fac {
+
+class Fac1 {
+    int num_aux1;
     public int ComputeFac(int num){
-        int num_aux;
-        int x;
-        num_aux = num_aux + 2;
         if (num < 1)
-            num_aux = 2;
+            num_aux1 = 2 + 8;
         else
-            num_aux = num * (this.ComputeFac(num-1));
-        return num_aux ;
+            num_aux1 = num * (this.ComputeFac(num-1));
+        return num_aux1;
     }
 }
+
+class Fac2 {
+    int num_aux2;
+    public int ComputeFac(int num){
+        if (num < 1)
+            num_aux2 = 2 + 8;
+        else
+            num_aux2 = num * (this.ComputeFac(num-1));
+        return num_aux2 ;
+    }
+}
+
+
