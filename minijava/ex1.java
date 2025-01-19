3f9bf5939cb3a01709f2b097c8070b857874cdf7
Lucas Serrano <lucas_serrano@id.uff.br>
2025-01-19 18:34:12 -0300

    atualização de erros e gitignore

--- minijava/ex1.java
+++ minijava/ex1.java
@@ -1,32 +1,21 @@
 class Factorial{
     public static void main(String[] a){
-        System.out.println(new Fac2().ComputeFac(10));
+        System.out.println(new Fac2().ComputeFac(10)); //Fac2 não existe
     }
 }
-
 class Fac {
     int num_aux1;
     public int ComputeFac(int num){
         int num_aux1;
         if (num < 1)
-            num_aux2 = 2 + 8;
+            num_aux2 = 2 + 8; //Variável num_aux2 não foi declarada
         else
-            num_aux1 = num * (this.ComputeFac(num-1, 7));
+            num_aux1 = num * (this.ComputeFac(num-1, 7)); //2 parâmetros são passados na função
         return num_aux1;
     }
 
-    public int ComputeFac(int num){
-        num_aux1 = num * (this.ComputeFac(56));
-        return num_aux1;
-    }
-    
-}
-
-class Fac1 {
-    int num_aux1;
-    public int ComputeFac(int num){
-        num_aux1 = num * (this.ComputeFac(true));
+    public int ComputeFac(int num){ //Função ComputeFac já foi declarada
+        num_aux1 = num * (this.ComputeFac(true)); //Parâmetro com tipo errado
         return num_aux1;
     }
 }
-
